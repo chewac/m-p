cd8af739d44ba7395cf4ea522797e73df6a11ce4
Kirill Pi. <38920548+chewac@users.noreply.github.com>
2026-01-23 22:27:53 +0100

    Update script.js

--- script.js
+++ script.js
@@ -11,93 +11,13 @@ const initializeObjectDetector = async () => {
             modelAssetPath: `https://storage.googleapis.com/mediapipe-models/object_detector/efficientdet_lite0/float16/1/efficientdet_lite0.tflite`,
             delegate: "GPU"
         },
-        scoreThreshold: 0.6,
+        scoreThreshold: 0.5,
         runningMode: runningMode
     });
     demosSection.classList.remove("invisible");
 };
 initializeObjectDetector();
-/********************************************************************
- // Demo 1: Grab a bunch of images from the page and detection them
- // upon click.
- ********************************************************************/
-const imageContainers = document.getElementsByClassName("detectOnClick");
-for (let imageContainer of imageContainers) {
-    imageContainer.children[0].addEventListener("click", handleClick);
-}
-/**
- * Detect objects in still images on click
- */
-async function handleClick(event) {
-    const highlighters = event.target.parentNode.getElementsByClassName("highlighter");
-    while (highlighters[0]) {
-        highlighters[0].parentNode.removeChild(highlighters[0]);
-    }
-    const infos = event.target.parentNode.getElementsByClassName("info");
-    while (infos[0]) {
-        infos[0].parentNode.removeChild(infos[0]);
-    }
-    if (!objectDetector) {
-        alert("Object Detector is still loading. Please try again.");
-        return;
-    }
-    // if video mode is initialized, set runningMode to image
-    if (runningMode === "VIDEO") {
-        runningMode = "IMAGE";
-        await objectDetector.setOptions({ runningMode: "IMAGE" });
-    }
-    const ratio = event.target.height / event.target.naturalHeight;
-    // objectDetector.detect returns a promise which, when resolved, is an array of Detection objects
-    const detections = objectDetector.detect(event.target);
-    displayImageDetections(detections, event.target);
-}
-function displayImageDetections(result, resultElement) {
-    const ratio = resultElement.height / resultElement.naturalHeight;
-    console.log(ratio);
-    for (let detection of result.detections) {
-        // Description text
-        const p = document.createElement("p");
-        p.setAttribute("class", "info");
-        p.innerText =
-            detection.categories[0].categoryName +
-                " - with " +
-                Math.round(parseFloat(detection.categories[0].score) * 100) +
-                "% confidence.";
-        // Positioned at the top left of the bounding box.
-        // Height is whatever the text takes up.
-        // Width subtracts text padding in CSS so fits perfectly.
-        p.style =
-            "left: " +
-                detection.boundingBox.originX * ratio +
-                "px;" +
-                "top: " +
-                detection.boundingBox.originY * ratio +
-                "px; " +
-                "width: " +
-                (detection.boundingBox.width * ratio - 10) +
-                "px;";
-        const highlighter = document.createElement("div");
-        highlighter.setAttribute("class", "highlighter");
-        highlighter.style =
-            "left: " +
-                detection.boundingBox.originX * ratio +
-                "px;" +
-                "top: " +
-                detection.boundingBox.originY * ratio +
-                "px;" +
-                "width: " +
-                detection.boundingBox.width * ratio +
-                "px;" +
-                "height: " +
-                detection.boundingBox.height * ratio +
-                "px;";
-        resultElement.parentNode.appendChild(highlighter);
-        resultElement.parentNode.appendChild(p);
-    }
-}
-/********************************************************************
- // Demo 2: Continuously grab image from webcam stream and detect it.
- ********************************************************************/
+
 let video = document.getElementById("webcam");
 const liveView = document.getElementById("liveView");
 let enableWebcamButton;
